8faa706d3e30fb2d83ff25980995409dc1f6b7eb
nicolaerastau <nickqsr@yahoo.com>
2018-10-25 22:09:40 +0200

    Updates

---
+++ precache-manifest.a038de0f283aa40da75f70d02bfec3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ed3b0a6dae40e93b473250ba716e88",
+    "url": "/react/static/media/staff.f1ed3b0a.png"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "644118321e573cf6b845",
+    "url": "/react/static/js/main.64411832.chunk.js"
+  },
+  {
+    "revision": "98c9b72f75b66db1ee70",
+    "url": "/react/static/js/1.98c9b72f.chunk.js"
+  },
+  {
+    "revision": "644118321e573cf6b845",
+    "url": "/react/static/css/main.68f5fe02.chunk.css"
+  },
+  {
+    "revision": "3b2128cef90139d4639ad7fe8031b323",
+    "url": "/react/index.html"
+  }
+];
